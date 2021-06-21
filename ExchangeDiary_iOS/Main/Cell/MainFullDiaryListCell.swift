90cc1fe970f8b6f2ef237087c8135a1861997d3a
tngusmiso <k1miso012@gmail.com>
2021-06-21 14:42:33 +0900

    💄(#3)[메인화면] MainGroupItemCell 추가

--- ExchangeDiary_iOS/Main/Cell/MainFullDiaryListCell.swift
+++ ExchangeDiary_iOS/Main/Cell/MainFullDiaryListCell.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class MainFullDiaryListCell: UITableViewCell {
+    let groupItemCellId = "MainGroupItemCell"
+//    let diaryItemCellId = ""
+    
     @IBOutlet weak var groupListCollectionView: UICollectionView!
     @IBOutlet weak var diaryListCollectionView: UICollectionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        groupListCollectionView.delegate = self
+        groupListCollectionView.dataSource = self
+        
+        groupListCollectionView.collectionViewLayout = {
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .horizontal
+            flowLayout.estimatedItemSize = CGSize(width: 20, height: 30)
+            flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+            flowLayout.minimumLineSpacing = 22
+            return flowLayout
+        }()
+        
+        let groupItemCellNib = UINib(nibName: groupItemCellId, bundle: nil)
+        groupListCollectionView.register(groupItemCellNib, forCellWithReuseIdentifier: groupItemCellId)
     }
 
     @IBAction func touchGroupManageButton(_ sender: Any) {
@@ -23,3 +40,23 @@ class MainFullDiaryListCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
 }
+extension MainFullDiaryListCell: UICollectionViewDelegate {
+}
+
+extension MainFullDiaryListCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch collectionView {
+        case groupListCollectionView:
+            guard let groupItemCell = groupListCollectionView.dequeueReusableCell(withReuseIdentifier: groupItemCellId, for: indexPath)
+                        as? MainGroupItemCell else { return MainGroupItemCell(frame: .zero) }
+            return groupItemCell
+        default:
+            return MainGroupItemCell(frame: .zero)
+        }
+    }
+}
+
