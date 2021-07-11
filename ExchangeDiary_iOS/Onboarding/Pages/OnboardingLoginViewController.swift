f079984aa9049f551a429896eac65cd9d6c7e011
tngusmiso <k1miso012@gmail.com>
2021-07-11 18:05:16 +0900

    🚧(#2)[카카오로그인] 카카오 버튼 액션 - 카카오톡 앱 설치 여부 확인 : 카카오톡으로 로그인 - 미 설치 시 : 웹으로 로그인(kakao.com) - 로그인 성공 시 : JWT 발급 - 로그인 실패 시 : 실패 alert 띄움

--- ExchangeDiary_iOS/Onboarding/Pages/OnboardingLoginViewController.swift
+++ ExchangeDiary_iOS/Onboarding/Pages/OnboardingLoginViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
 
 class OnboardingLoginViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,5 +22,40 @@ class OnboardingLoginViewController: UIViewController {
     }
     @IBAction func touchKakaoLogin(_ sender: Any) {
         print("카카오로그인")
+        kakaoLogin()
+    }
+
+    // MARK: - Kakao Login
+    private func kakaoLogin() {
+        if UserApi.isKakaoTalkLoginAvailable() { // 카카오톡 앱 설치 여부
+            UserApi.shared.loginWithKakaoTalk(completion: kakaoLoginCompletion) // 카카오톡으로 로그인
+        } else {
+            UserApi.shared.loginWithKakaoAccount(completion: kakaoLoginCompletion) // 웹으로 로그인
+        }
+    }
+    
+    // 카카오 로그인 결과에 따른 completion 클로저
+    private lazy var kakaoLoginCompletion: (OAuthToken?, Error?) -> Void = { [weak self] (oauthToken, error) in
+        guard let oauthToken = oauthToken else {
+            self?.failedKakaoLogin(alert: error?.localizedDescription)
+            return
+        }
+        let token = oauthToken
+        self?.successKakaoLogin(token: token)
+    }
+    
+    private func failedKakaoLogin(alert message: String?) {
+        // 로그인 실패 메세지 alert
+    }
+    
+    private func successKakaoLogin(token oauthToken: OAuthToken) {
+        _ = getJWT(with: oauthToken)
+        // 키체인에 토큰 저장
+        // 화면 전환
+    }
+    
+    private func getJWT(with oauthToken: OAuthToken) -> String {
+        // JWT 발급 api 호출
+        return "JWT"
     }
 }
